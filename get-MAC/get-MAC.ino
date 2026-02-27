eb093c644845733e344bed5ad8b0de170f650081
Eduardo Menchaca <eduardomenchaca01@gmail.com>
2026-02-27 03:04:36 -0600

    new(get-MAC): Conseguir el MAC address de ESP-32

---
+++ get-MAC/get-MAC.ino
@@ -0,0 +1,11 @@
+#include <WiFi.h>
+
+void setup() {
+  Serial.begin(115200);
+  WiFi.mode(WIFI_MODE_STA);
+  delay(1000);
+  Serial.print("MAC Address: ");
+  Serial.println(WiFi.macAddress());
+}
+
+void loop() {}
